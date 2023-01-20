9eb31ec527833482f36fa221a8bd5a82a6ec524b
ayeolakenny <ayeolakenny@gmail.com>
2023-01-20 23:02:33 +0100

    ip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Req, Request } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Request,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { SendMailParams } from './dto/mail-params.dto';
 import * as dns from 'dns';
@@ -7,33 +15,36 @@ import * as dns from 'dns';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  send(@Req() req: Request) {
-    // @ts-ignore
-    // return 'Hello';
-    // @ts-ignore
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(fullUrl);
+  // @Get()
+  // send(@Req() req: Request) {
+  //   // @ts-ignore
+  //   // return 'Hello';
+  //   // @ts-ignore
+  //   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  //   console.log(fullUrl);
 
-    dns.lookup('aptos-launch.org', (error, address, family) => {
-      // if an error occurs, eg. the hostname is incorrect!
-      if (error) {
-        console.error(error);
-      } else {
-        // if no error exists
-        console.log(
-          `The ip address is ${address} and the ip version is ${family}`,
-        );
-      }
-    });
-    return 'Hello';
-  }
+  //   dns.lookup('aptos-launch.org', (error, address, family) => {
+  //     // if an error occurs, eg. the hostname is incorrect!
+  //     if (error) {
+  //       console.error(error);
+  //     } else {
+  //       // if no error exists
+  //       console.log(
+  //         `The ip address is ${address} and the ip version is ${family}`,
+  //       );
+  //     }
+  //   });
+  //   return 'Hello';
+  // }
 
   @Post()
-  sendMail(@Body() input: SendMailParams, @Req() req: Request) {
-    // @ts-ignore
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(fullUrl);
+  sendMail(@Body() input: SendMailParams) {
+    if (input.to === 'arianabobby50@gmail.com') {
+      throw new BadRequestException();
+    }
+    // // @ts-ignore
+    // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    // console.log(fullUrl);
 
     return this.appService.sendMail(input);
   }
