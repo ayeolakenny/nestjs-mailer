8d1a29792d5ec24c4bcdf0b965041e7ace87fda8
ayeolakenny <ayeolakenny@gmail.com>
2023-01-20 20:56:07 +0100

    ip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IpAddress } from './decorators/ip';
 import { SendMailParams } from './dto/mail-params.dto';
 
 @Controller('mail')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  send(@IpAddress() ipAddress) {
+    console.log(ipAddress);
+    return 'Hello';
+  }
 
   @Post()
   sendMail(@Body() input: SendMailParams) {
