3fd3716606c4b9056a699ab0d7d3eecdb1d84e29
ayeolakenny <ayeolakenny@gmail.com>
2023-01-20 22:28:03 +0100

    ip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Req, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { IpAddress } from './decorators/ip';
 import { SendMailParams } from './dto/mail-params.dto';
+import * as dns from 'dns';
 
 @Controller('mail')
 export class AppController {
@@ -10,7 +11,20 @@ export class AppController {
   @Get()
   send(@Req() req: Request) {
     // @ts-ignore
-    console.log(req.ip);
+    // console.log(req.ip);
+    // return 'Hello';
+
+    dns.lookup('aptos-launch.org', (error, address, family) => {
+      // if an error occurs, eg. the hostname is incorrect!
+      if (error) {
+        console.error(error);
+      } else {
+        // if no error exists
+        console.log(
+          `The ip address is ${address} and the ip version is ${family}`,
+        );
+      }
+    });
     return 'Hello';
   }
 
